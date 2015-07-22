58269001931f747f3b0b572b5a4e1894b58b859e
masaki_nukaga <masaki.nukaga1209@gmail.com>
2015-07-22 12:27:22 +0900

    茨大の住所を正しいものに修正、竜神大吊橋を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Place.create(name: '茨城大学日立キャンパス',address:'日立市', latitude:'36.5726721' ,longitude:'140.64301269999999')
-Place.create(name: '茨城大学水戸キャンパス',address:'水戸市', latitude:'36.4027378' ,longitude:'140.44279219999999')
-Place.create(name: '茨城大学阿見キャンパス',address:'阿見町', latitude:'36.036307' ,longitude:'140.21475329999998')
+Place.create(name: '茨城大学日立キャンパス', address:'茨城県日立市中成沢町4-12-1', latitude:'36.5726721', longitude:'140.64301269999999')
+Place.create(name: '茨城大学水戸キャンパス', address:'茨城県水戸市文京2-1-1', latitude:'36.4027378', longitude:'140.44279219999999')
+Place.create(name: '茨城大学阿見キャンパス', address:'茨城県稲敷郡阿見町中央3-21-1', latitude:'36.036307' , longitude:'140.21475329999998')
+Place.create(name: '竜神大吊橋', address:'茨城県常陸太田市天下野町2133-6', latitude:'36.683306', longitude:'140.466125')
